951d33fc026fd8c943e57410e88c65d8dffa42bb
rhea <rthakur@bu.edu>
2026-09-25 18:25:19 -0400

    implemented mutexes

--- mutex_aggregator.go
+++ mutex_aggregator.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"net/rpc"
+	"math"
+	"sync"
+	"time"
 )
 
 // Mutex-based aggregator that reports the global mode temperature periodically.
@@ -19,5 +22,48 @@ func mutexAggregator(
 	quit chan struct{},
 	client *rpc.Client,
 ) {
-	// TODO: Your code here.
+	period := time.Duration(averagePeriod * float64(time.Second))
+
+	for {
+		counts := make(map[float64]int)
+		var mu sync.Mutex
+		
+		timer := time.NewTimer(period)
+
+		for i := 0; i < k; i++ {
+			go func(stationID int) {
+				temp, err := GetWeatherData(client, stationID)
+				if err == nil {
+					mu.Lock()
+					counts[temp]++
+					mu.Unlock()
+				}
+			}(i + 1) // Station IDs are 1-indexed
+		}
+
+		select {
+		case <-timer.C:
+			// Calculate the mode and its count
+			mu.Lock()
+			mode := math.NaN()
+			maxCount := 0
+			for temp, count := range counts {
+				if count > maxCount || (count == maxCount && temp < mode){
+					mode = temp
+					maxCount = count
+				}
+			}
+			mu.Unlock()
+
+			select {
+			case out <- [2]float64{mode, float64(maxCount)}:
+			case <-quit:
+				return
+			}
+
+		case <-quit:
+			timer.Stop()
+			return
+		}
+	}
 }
